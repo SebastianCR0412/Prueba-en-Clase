909ec1ba381ea160464568c266c3cfbcc2f173d6
Sebastian <sebastian.castro.rengifo@correounivalle.edu.com>
2024-02-29 10:59:35 -0500

    mensaje

---
+++ Clase#1/src/App.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola Mundo :)");
+        JOptionPane.showMessageDialog(null, "Ya tengo hambre xdxd");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Prueba-en-Clase
+hola mundo
